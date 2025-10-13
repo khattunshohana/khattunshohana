63b09aab716cf87ef1806b4f127b6a2086198519
khattunshohana <khattunshohana41@gmail.com>
2025-10-13 02:51:22 -0700

    khattunshohana1
    khattunshohana1

---
+++ khattunshohana1.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "java programming";
+        System.out.println("Length: " + text.length());
+        System.out.println("Uppercase: " + text.toUpperCase());
+    }
+}
