3b95e2267976084dd37ba6cbad7849902286a91d
khattunshohana <khattunshohana41@gmail.com>
2025-10-14 04:19:24 -0700

    khattunshohana41
    khattunshohana41

---
+++ khattunshohana41.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string name = "C++";
+    cout << "Length: " << name.length() << endl;
+    cout << "Uppercase: " << name << endl;
+    return 0;
+}
